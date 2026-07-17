9f0cf19977e153faadb00e6f984facbd70dcbd2f
Manju906 <manjuharinikolanti@gmail.com>
2026-07-17 11:29:18 +0530

    Add files via upload

---
+++ conditionalstatements.py
@@ -0,0 +1,129 @@
+#if-condition by using comparision operators
+#<,>,<=,>=,!=,==
+'''a=10
+b=15
+if a<b:
+    print("true")'''
+'''a=6
+b=9
+if a>b:
+    print("true")'''
+'''a=12
+b=14
+if a<=b:
+    print("true")'''
+'''a=10
+b=15
+if a>=b:
+    print("true")'''
+'''a=20
+b=40
+if a!=b:
+    print("not equal")'''
+'''a=5
+b=5
+if a==b:
+    print("equal")'''
+'''a=9
+if a>3:
+    print("greater")'''
+#strings
+#for strings we should use only ==
+'''a="python"
+if a=="python":
+    print("equal")'''
+'''a="java"
+if a!="python":
+    print("equal")'''
+'''a=int(input("a value "))
+b=int(input("b value "))
+if a<b:
+      print("less")'''
+'''a=int(input("a value "))
+if a>40:
+    print("greater")'''
+'''a=input("data")
+if a=="python":
+    print("true")'''
+#if-condition by using logical operators
+#and,or,not
+'''a=5
+b=7
+if a<b and b>a:
+    print("true")'''
+'''a=4
+b=6
+if a!=b and b!=a:
+    print("false")'''
+'''a=8
+b=12
+if a<b or b>a:
+    print("true")'''
+'''a=8
+b=12
+if a!=b or b!=a:
+    print("true")'''
+'''a=5
+b=9
+if not a<b: #not will be used before declaring variables in if line
+    print("less")'''
+'''a=5
+b=9
+if not a>b:
+    print("less")'''
+'''a=5
+b=9
+if not a<b and b>a:
+    print("less")'''
+#if condition by using identify operators
+#is,is not
+'''a=7
+if(type(a)) is int:
+   print("it is not")'''
+'''a=7
+if(type(a)) is  not int:
+   print("false")'''
+'''a=int(input("a value "))
+if type(a) is int:
+    print("true")'''
+'''a=float(input("a value "))
+if (type(a)) is float:
+    print("True")'''
+#if condition by using membership operators
+#in,not in
+'''a=2,3,4,5,6,7,8,9,10
+if 10 in a:
+    print("True")'''
+'''=2,3,4,5,6,7,8,9,10
+if 11  not in a:
+    print("False")'''
+'''a=int(input("a value "))
+if 30 in a:
+    print("true")''' #error #in run time we cant use m operators
+'''a=[4,5,6,7,8,9,10,11]
+b=int(input("value "))
+if b in a:
+    print("true")'''
+#if else conditions by using comparision operators
+'''a=3
+b=6
+if a<b:
+    print("less")
+else:
+    print("true")'''
+'''a=3
+b=6
+if a>b:
+    print("less")
+else:
+    print("true")''' #we cannot pass anything except colon in else line
+
+'''a=7
+b=9
+if a>b:
+    print("less")
+else:
+    print("true")'''
+
+
+    
