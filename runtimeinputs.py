4a3e7cf1909464e85c183a673fbc7ef636627717
Manju906 <manjuharinikolanti@gmail.com>
2026-09-09 21:16:56 +0530

    Add files via upload

---
+++ runtimeinputs.py
@@ -0,0 +1,94 @@
+#runtimeinput
+#int
+'''a=int(input("enter  the a value"))
+b=int(input("enter the b value"))
+print(a+b)'''
+#float
+'''a=float(input("enter  the a value"))
+b=float(input("enter the b value"))
+print(a+b)'''
+#str1
+'''a=str(input("data1"))
+b=str(input("data2"))
+print(a+b)'''
+#str2
+'''a=input("data1")
+b=input("data2")
+print(a+b)'''
+#username
+'''fname=input("firstname")
+lname=input("lastname")
+print((fname+" "+lname).title())'''
+#complex
+'''a=complex(input("a val"))
+b=complex(input("b val"))
+print(a+b)'''
+#bool
+'''a=bool(input("data1"))
+b=bool(input("data2"))
+c=bool(input("data3"))
+print(a+b+c)'''
+#eval-accepts any datatype
+'''a=eval(input("data1"))
+b=eval(input("data2"))
+print(a+b)'''
+#options1
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input("chose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a-b)
+print(a*b)#all will print because we have all in print'''
+#2
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=int(input(chose the option
+                 1.add
+                 2.sub
+                 3.mul))
+print(a+b)
+print(a-b)
+print(a*b)'''
+#3
+'''a=int(input("a value"))
+b=int(input("b value"))
+option=input(chose the option
+                 add
+                 sub
+                 mul)
+print(a+b)
+print(a-b)
+print(a*b)'''
+#task
+'''a=[9,1,5,2,8,4,6,3,7,0]
+#o/p:[7,6,4,3,0,9,8,5,2,1]
+n=len(a)//2
+a1=a[:n]
+a2=a[n:]
+a1.sort()
+a2.sort()
+a1.reverse()
+a2.reverse()
+print(a2+a1)'''
+#OR
+a=[9,1,5,2,8,4,6,3,7,0]
+#o/p:[7,6,4,3,0,9,8,5,2,1]
+a1=a[:5]
+a2=a[5:]
+a1.sort()
+a2.sort()
+a1.reverse()
+a2.reverse()
+print(a2+a1)
+#swapping
+'''a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print("swapping of two numbers is a=%s,b=%s"%(a,b))'''
+'''a="manju"
+b="harini"
+a,b=b,a
+print("swapping of two numbers is a=%s,b=%s"%(a,b))'''
+
