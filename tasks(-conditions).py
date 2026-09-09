17702ef4a303ca5f24408b291933cfe194707c97
Manju906 <manjuharinikolanti@gmail.com>
2026-09-09 21:17:52 +0530

    Add files via upload

---
+++ tasks(-conditions).py
@@ -0,0 +1,95 @@
+#tasks
+#even or odd
+'''n=int(input("Enter n value:"))
+if n%2==0:
+    print("It is Even")
+else:
+    print("Is is odd")'''
+#voting
+'''age=int(input("Enter age:"))
+if age>=18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")'''
+#leap year
+'''year=int(input("Enter year:"))
+if year%4==0:
+    print("Leap Year")
+else:
+    print("Not a leap year")'''
+#guest code
+'''name=input("Enter the name:").lower()#according to the condition format
+if name=="manju":
+    print("Welcome manju")
+else:
+    print("Welcome guest")'''
+#multiple guest code
+'''names=["sneha","kavya","priya","preethi","bhanu"]
+a=input("Enter the name")
+if a in names:
+    print("Welcome",a)
+else:
+    print("Welcome guest")'''
+#vowels
+'''vowels=['a','e','i','o','u']
+a=input("enter the letter:")
+if a in vowels:
+    print("Vowel")
+else:
+    print("Consonant")'''
+#bakery
+'''price=int(input("Enter the price:"))
+if price==1200:
+    print("Red Velvet")
+elif price==1000:
+    print("Almond Cake")
+elif price==800:
+    print("Choclate Cake")
+elif price==600:
+    print("Butterscotch")
+else:
+    print("Sorry cake is not available")'''
+#pizza
+'''name=input("Enter the pizza name:").lower()
+if name=="crispy chicken pizza":
+    print("800")
+elif name=="bbq pizza":
+    print("600")
+elif name=="panner pizza":
+    print("400")
+elif name=="cheese corn":
+    print("300")
+elif name=="french fries & coke":
+    print("200")'''
+
+'''age=int(input("enter the age:"))
+attendance=int(input("enter the attendance:"))
+marks=int(input("enter the marks:"))
+if age>=18:
+        print("Eligible for scholorship")
+if attendance>=90:
+        print("Allowed to write exams")
+if marks>=80:
+        print("Eligible for higher studies")'''
+
+'''username="pooja"#nested if and if else
+pwd=1234 if true
+print("login succesful")
+else invalid credentials'''
+
+'''username=input("Enter user name:")
+pwd=input("Enter your pwd:")
+if username=="pooja":
+    if pwd=="1234":
+        print("Login successful")
+    else:
+        print("Invalid password")
+else:
+    print("Invalid username")'''
+
+'''username=input("Enter user name:")
+pwd=input("Enter your pwd:")
+if username=="pooja" and pwd=="1234":
+    print("Login successful")
+else:
+    print("Login Failed")'''
