5a27d9809ba08a2126fa6613829bd9040cb70585
Manju906 <manjuharinikolanti@gmail.com>
2026-09-10 10:51:31 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,16 @@
+'''import modules
+modules.greetings("manju")'''
+
+#import modules
+
+#import modules
+
+'''import modules
+a=modules.details["idnos"]
+b=modules.details["names"]
+#print(a,b)
+print(a)
+print(b)'''
+
+
+import modules 
---
+++ modules.py
@@ -0,0 +1,60 @@
+#diff btwn modules,lib,package
+#modules
+#A module is a single python file it consists python code
+#A module contains functions,classes,variables
+#examples of modules such as math.py,random.py,and mymodule.py
+
+
+#package
+#It is a collection of modules 
+#examples of packages such as numpy,pandas and matplotlib
+
+#library
+#A library consists of both modules and packages
+#examples of libraries such as numpy,request
+
+
+#note
+#Every python file is a module and import is a keyword
+#Every python file is saved internally with variable name as main
+
+'''def greetings(name):
+    print("Welcome",name)'''
+
+
+'''a=10
+b=20
+print(a+b)'''
+
+
+'''a=input("fname")
+b=input("lname")
+print(a+" "+b)'''
+
+
+'''details={"idnos":[10,20,30],
+         "names":["divya","bhavya","surya"],
+         "cities":["chennai","hyderabad","vizag"]}'''
+
+
+'''if __name__=="__main__":
+    a=[10,20,30,40,50]
+    #a.append("code")
+    a.extend("code")
+    print(a)'''
+
+
+'''def dummy():
+    if __name__=="__main__":
+        print("this program is run as script")
+    else:
+        print("this program is run as module")
+dummy()'''
+
+
+
+
+
+
+
+
